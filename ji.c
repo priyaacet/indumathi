142d55d203578e314c96e8ac6aafec189b76ae53
priyaacet <indumathis675@gmail.com>
2017-07-13 09:13:08 +0530

    Update ji.c

--- ji.c
+++ ji.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+  scanf("%d%d",&a,&b);
+  if(a>b)
+  {
+    printf(" The num is postive");
+  }
+  else
+  {
+   printf("thr num is negative");
+  }
+  getch();
+}
