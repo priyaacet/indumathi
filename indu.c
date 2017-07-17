15ef5b68a629f54270ddf5122e1b86da82b991ab
priyaacet <indumathis675@gmail.com>
2017-07-17 12:18:18 +0530

    Create indu.c

---
+++ indu.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a/2==0)
+{
+printf("the num is even");
+}
+else
+{
+printf(" the num is odd");
+}
+return 0;
+}
