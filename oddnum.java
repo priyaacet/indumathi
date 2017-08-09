25e7a8b37320d0bcf964c6397e9fc3f7afef8580
priyaacet <indumathis675@gmail.com>
2017-08-09 11:14:21 +0530

    Create oddnum.java

---
+++ oddnum.java
@@ -0,0 +1,13 @@
+imort java.io.*;
+import java.util.*;
+import java.net;
+class C
+{
+public static void main(String args[])
+{
+int a;
+Scanner a=new Scanner(System.in);
+b=a.nextInt();
+if(a%2=0)
+{
+System .out.println("
