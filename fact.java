253bb6a1a4b91eb030efa36b7814a97e7e62baed
priyaacet <indumathis675@gmail.com>
2017-08-09 11:00:45 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+class B
+{
+public static void main(String args[])
+{
+int i=5;
+int n=1;
+int fact;
+for(i=1;i<n;i++)
+{
+fact=fact*i;
+System.out.println("factorial is"+fact);
+}
+}
+}
