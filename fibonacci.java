3a6b853e73fca7c2ffac7833cb9b8b81868bd464
priyaacet <indumathis675@gmail.com>
2017-08-10 14:29:35 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+class D
+{
+public static void main(string args[])
+{
+int f1=0;f2=1;
+int f;
+int n=5;
+for(i=0;i<=n;i++)
+{
+system.out.println("fibbonacci series"+f1);
+f=f1+f2;
+f1=f2;
+f2=f;
+}
+}
+}
+
