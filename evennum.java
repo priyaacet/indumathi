5dc2438a0874a62ced260cb8e8907643d5b19bef
priyaacet <indumathis675@gmail.com>
2017-08-10 14:16:15 +0530

    Create evennum.java

---
+++ evennum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class C
+{
+public static void main(String args[])
+{
+int b,c,i;
+Scanner a=new Scanner(System.in);
+b=a.nextInt();
+c=a.nextInt();
+for(i=b;i<=c;i++)
+
+if(i%2==0)
+{
+System .out.println(i);
+}
+}
+}
