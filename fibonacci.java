30a546fa8b58268873562790d5a1faeff8b8bf03
priyaacet <indumathis675@gmail.com>
2017-08-10 14:48:07 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -3,18 +3,17 @@ import java.util.*;
 import java.net.*;
 class D
 {
-public static void main(string args[])
+public static void main(String args[])
 {
-int f1=0;f2=1;
+int f1=0,f2=1,i;
 int f;
 int n=5;
 for(i=0;i<=n;i++)
 {
-system.out.println("fibbonacci series"+f1);
+System.out.println("fibbonacci series"+f1);
 f=f1+f2;
 f1=f2;
 f2=f;
 }
 }
 }
-
