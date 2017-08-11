e9ac69682d45cecbd161dfca3e9fc7fdfa29db62
priyaacet <indumathis675@gmail.com>
2017-08-11 12:44:52 +0530

    Create prime1.java

---
+++ prime1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+class AA
+{
+public static void main(String args[])
+{
+int i=1;
+int b=1;
+int n=10;
+n=b/2;
+for(i=1;i<=n;i++);
+{
+if(b%i==0);
+{
+System.out.println(b);
+}
+}
+}
+}
