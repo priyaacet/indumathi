539f79450fcb51505b2e29f66a5a90e1b9ef4447
priyaacet <indumathis675@gmail.com>
2017-08-23 10:51:58 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -4,7 +4,24 @@ class BB
 {
 public static void main(String args[])
 {
-int r,sum=0.temp;
+int r,sum=0,temp;
 int n;
 Scanner a=new Scanner(System.in)
 b=a.nextInt();
+temp=n;
+  while(n!=0);
+  {
+    r=n%10;
+    sum=sum*10+r;
+    n/=10;
+  }
+  if(temp==sum)
+  {
+    System.out.println(temp+"is palindrome");
+  }
+  else
+  {
+    System.out.println(temp+"is not palindrome");
+  }
+}
+}
