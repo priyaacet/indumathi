fef96f4f1cafaa14391336c0da3ecea26541d6b0
priyaacet <indumathis675@gmail.com>
2017-08-23 11:04:58 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,27 +1,27 @@
 import java.io.*;
-import java.net.*;
-class BB
+import java.util.*;
+ public class pal
 {
 public static void main(String args[])
 {
 int r,sum=0,temp;
 int n;
 Scanner a=new Scanner(System.in);
-b=a.nextInt();
+n=a.nextInt();
 temp=n;
-  while(n!=0);
+  while(n!=0)
   {
     r=n%10;
     sum=sum*10+r;
     n/=10;
   }
   if(temp==sum)
-  {
+  
     System.out.println(temp+"is palindrome");
-  }
+  
   else
-  {
+  
     System.out.println(temp+"is not palindrome");
-  }
+  
 }
 }
