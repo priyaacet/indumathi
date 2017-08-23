fdb04b5b85234f9b4bdf1bd02dd5977188ca509a
priyaacet <indumathis675@gmail.com>
2017-08-23 11:13:44 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.util.*;
-public class first
+ class res
 {
 public static void main(String args[])
 {
 int a=123,ans=0;
-while(a!=0);
+while(a!=0)
 {
 int rem=a%10;
 ans=ans*10+rem;
