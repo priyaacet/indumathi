221f2287bbef3920597fa9411037bab667a3ea4a
priyaacet <indumathis675@gmail.com>
2017-08-31 09:12:04 +0530

    Create fact2.java

---
+++ fact2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class factorial
+{
+public void main(String args[])
+{
+int n=10;
+int a;
+int fact=2;
+for(a=2;a<=n;a++)
+{
+fcat=a*fact;
+}
+Sysem.out.println("fact of given number is'+fact);
+}
+}
