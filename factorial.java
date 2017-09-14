f0c36d48aca68801a1ed0cd7bcf1a0f55b51e8fc
priyaacet <indumathis675@gmail.com>
2017-09-14 14:22:01 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,1 +1,17 @@
-
+import java.io.*;
+import java.util.*;
+import java.net.*;
+class B
+{
+public static void main(String args[])
+{
+int n=5;
+int i;
+int fact=1;
+for(i=1;i<n;i++)
+{
+fact=i*fact;
+System.out.println("factorial is"+fact);
+}
+}
+}
