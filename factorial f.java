7823259166dc567fa1da71c4453139f47250174b
priyaacet <indumathis675@gmail.com>
2017-09-18 12:23:41 +0530

    Update factorial f.java

--- factorial f.java
+++ factorial f.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 class fact
 {
+  public static void main(String args[])
+  {
 int i;
 fac=1;
 int n=5;
@@ -10,4 +12,5 @@ for(i=1;i<=n;i++)
 fact=fact*i;
 }
 System.ou.println("factorial is"+fact);
+}
 }
