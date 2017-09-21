a54bf791878178d4b9894d088e9b0bf75eefb8d5
priyaacet <indumathis675@gmail.com>
2017-09-21 09:15:05 +0530

    Create reverse digit.c

---
+++ reverse digit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main
+{
+int n,temp,re,rev=0;
+printf("enter positive number");
+scanf("%d",&n);
+temp=n;
+while(temp>o)
+{
+re=temp/10;
+rev=rev*10+re
+temp/=10;printf("The reverse of %d is %d.", number, reverse);
+}
