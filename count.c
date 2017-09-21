3ad31458ff01bb87f0e3b54e251461e4d5cc3d5c
priyaacet <indumathis675@gmail.com>
2017-09-21 09:01:08 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+long long n;
+int count=0;
+printf("enter the number og integer");
+scanf(%lld",&n);
+while(n!=0)
+{
+n/=10;
+++count;
+}
+printf("number of digits%d",count);
+}
